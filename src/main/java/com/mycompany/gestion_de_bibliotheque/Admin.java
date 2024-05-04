baeadeea8b84d328f41ebabb0cd719bde355d596
Wissal AIT HEMAD <128430073+wissalah@users.noreply.github.com>
2024-05-04 18:53:09 +0200

    Update Admin.java

--- src/main/java/com/mycompany/gestion_de_bibliotheque/Admin.java
+++ src/main/java/com/mycompany/gestion_de_bibliotheque/Admin.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.gestion_de_bibliotheque;
 
-/**
- *
- * @author user
- */
-class Admin extends Personne {
+public class Admin extends Personne {
     public Admin(String nom, String password) {
-        super(nom, "admin", password);
+        super(nom, "admin", password, null); // Assuming no email for admin
     }
 }
