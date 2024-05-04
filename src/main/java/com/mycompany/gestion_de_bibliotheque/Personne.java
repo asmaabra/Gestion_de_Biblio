44596d24bce16b6ac5232617487a8b15def9a78c
Wissal AIT HEMAD <128430073+wissalah@users.noreply.github.com>
2024-05-04 18:52:44 +0200

    Update Personne.java

--- src/main/java/com/mycompany/gestion_de_bibliotheque/Personne.java
+++ src/main/java/com/mycompany/gestion_de_bibliotheque/Personne.java
@@ -4,33 +4,42 @@ public class Personne {
     private String nom;
     private String password;
     private String role;
+    String email;
 
-    public Personne(String nom, String role, String password) {
+    public Personne(String nom, String email, String password, String role) {
         this.nom = nom;
+        this.email = email;
+        this.password = password;
         this.role = role;
-        this.password=password;
     }
 
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void setRole(String role) {
         this.role = role;
     }
 
-    // Getters et Setters pour les attributs nom et role
 
     public String getNom() {
         return nom;
     }
 
-    public String getRole() {
-        return role;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRole() {
+        return role;
     }
 
     public void setPassword(String password) {
