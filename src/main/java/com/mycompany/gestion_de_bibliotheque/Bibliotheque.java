f4ba0c0546f9934634469737f1afa57e2342fc03
Wissal AIT HEMAD <128430073+wissalah@users.noreply.github.com>
2024-05-04 14:56:34 +0200

    Update Bibliotheque.java

--- src/main/java/com/mycompany/gestion_de_bibliotheque/Bibliotheque.java
+++ src/main/java/com/mycompany/gestion_de_bibliotheque/Bibliotheque.java
@@ -1,53 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.gestion_de_bibliotheque;
-
-/**
- *
- * @author asma
- */
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Bibliotheque {
-    private List<Livre> livres;
+    private List<Livre> catalogue;
     private List<Emprunt> emprunts;
+    private List<User> utilisateurs;
 
     public Bibliotheque() {
-        livres = new ArrayList<>();
-        // Initialiser les livres...
-        livres.add(new Livre("Titre 1", "Auteur 1", true));
-    livres.add(new Livre("Titre 2", "Auteur 2", false));
-    livres.add(new Livre("Titre 3", "Auteur 3", true));
-        emprunts = new ArrayList<>();
+        this.catalogue = new ArrayList<>();
+        this.emprunts = new ArrayList<>();
+        this.utilisateurs = new ArrayList<>();
     }
 
-   
-       
+    public void ajouterLivre(Livre livre) {
+        catalogue.add(livre);
+    }
 
-public List<Livre> getLivresDisponibles() {
-    List<Livre> livresDisponibles = new ArrayList<>();
-    for (Livre livre : livres) {
-        if (livre.isDisponible()) {
-            livresDisponibles.add(livre);
+    public void supprimerLivre(Livre livre) {
+        catalogue.remove(livre);
+    }
+
+    public void ajouterEmprunt(Emprunt emprunt) {
+        emprunts.add(emprunt);
+    }
+
+    public void supprimerEmprunt(Emprunt emprunt) {
+        emprunts.remove(emprunt);
+    }
+
+    public List<Livre> getLivresDisponibles() {
+        List<Livre> livresDisponibles = new ArrayList<>();
+        for (Livre livre : catalogue) {
+            if (livre.isDisponible()) {
+                livresDisponibles.add(livre);
+            }
         }
+        return livresDisponibles;
     }
-    return livresDisponibles;
-} 
-    
-    public void emprunterLivre(Livre livre, Date dateEmprunt, Date dateRetour) {
+
+    public void emprunterLivre(Livre livre, User user, Date dateEmprunt, Date dateRetour) {
         livre.setDisponible(false);
         emprunts.add(new Emprunt(livre, dateEmprunt, dateRetour));
     }
-
-    public void retournerLivre(Emprunt emprunt) {
-        emprunts.remove(emprunt);
-        emprunt.getLivre().setDisponible(true);
-    }
 }
-
-
