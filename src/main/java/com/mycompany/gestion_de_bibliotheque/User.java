0f803d294c317a98070badfbbc633d121423ad86
Wissal AIT HEMAD <128430073+wissalah@users.noreply.github.com>
2024-05-04 18:52:13 +0200

    Update User.java

--- src/main/java/com/mycompany/gestion_de_bibliotheque/User.java
+++ src/main/java/com/mycompany/gestion_de_bibliotheque/User.java
@@ -1,20 +1,8 @@
 package com.mycompany.gestion_de_bibliotheque;
 
-/**
- *
- * @author user
- */
 public class User extends Personne {
    
-
-    public User(String nom, String role, String password) {
-        super(nom, password, role);
-       
+    public User(String nom, String email, String password, String role) {
+        super(nom, email, password, role);
     }
-
-    // Getters and setters for the username and password attributes
-
-    
-
-   
 }
