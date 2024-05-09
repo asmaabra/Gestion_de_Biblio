a0451f0f2766eda3087ad1b2b4717d00a146684d
asmaabra <153956277+asmaabra@users.noreply.github.com>
2024-05-09 06:53:32 +0100

    Update Gestion_De_Bibliotheque.java

--- src/main/java/com/mycompany/gestion_de_bibliotheque/Gestion_De_Bibliotheque.java
+++ src/main/java/com/mycompany/gestion_de_bibliotheque/Gestion_De_Bibliotheque.java
@@ -4,13 +4,24 @@
 
 package com.mycompany.gestion_de_bibliotheque;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author asma
  */
 public class Gestion_De_Bibliotheque {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+ public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Accueil());
     }
-}
+
+           
+
+    }
